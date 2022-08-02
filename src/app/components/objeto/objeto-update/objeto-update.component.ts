dddd20ed96ec7775ba35e5fd9deaf72786c853d6
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-08-02 16:30:49 -0300

    Função Objeto-update implantada

--- src/app/components/objeto/objeto-update/objeto-update.component.ts
+++ src/app/components/objeto/objeto-update/objeto-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Objeto } from 'src/app/models/objeto';
+import { ObjetoService } from 'src/app/services/objeto.service';
 
 @Component({
   selector: 'app-objeto-update',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ObjetoUpdateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  objeto: Objeto = {
+    id:         '',
+    descricao:       ''
   }
 
+  descricao: FormControl =  new FormControl(null, Validators.minLength(3));
+  
+  constructor(
+    private service: ObjetoService,
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
+
+  ngOnInit(): void {
+    this.objeto.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.objeto.id).subscribe(resposta => {
+      this.objeto = resposta;
+    })
+  }
+
+  update(): void {
+    this.service.update(this.objeto).subscribe(() => {
+      this.toast.success('Objeto atualizado com sucesso', 'Update');
+      this.router.navigate(['objeto'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+  
+  validaCampos(): boolean {
+    return this.descricao.valid
+  }
 }
