01905dc70f45ed8436463abd6a781762712a182e
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-08-02 16:10:34 -0300

    Função Segmento-create implantada

--- src/app/components/segmento/segmento-create/segmento-create.component.ts
+++ src/app/components/segmento/segmento-create/segmento-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Segmento } from 'src/app/models/segmento';
+import { SegmentoService } from 'src/app/services/segmento.service';
 
 @Component({
   selector: 'app-segmento-create',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SegmentoCreateComponent implements OnInit {
 
-  constructor() { }
+  segmento: Segmento = {
+    id:         '',
+    descricao:  ''
+  }
+
+  descricao: FormControl = new FormControl(null, Validators.minLength(3));
+
+  constructor(
+    private service: SegmentoService,
+    private toast: ToastrService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  
+  create(): void {
+    this.service.create(this.segmento).subscribe(() => {
+      this.toast.success('Segmento cadastrado com sucesso', 'cadastro');
+      this.router.navigate(['segmento'])
+    }, ex => {
+      console.log(ex);
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
+  }
+
+  validaCampos(): boolean {
+    return this.descricao.valid
+  }
+  
 }
+
