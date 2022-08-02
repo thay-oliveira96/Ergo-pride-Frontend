0595bb9a89591786f2201af17ba6ddfb0b75cae5
Thaynan Oliveira <thaynan-rodrigues@outlook.com>
2022-08-02 16:42:08 -0300

    Função Segmento-delete implementada

--- src/app/components/segmento/segmento-delete/segmento-delete.component.ts
+++ src/app/components/segmento/segmento-delete/segmento-delete.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Segmento } from 'src/app/models/segmento';
+import { ObjetoService } from 'src/app/services/objeto.service';
+import { SegmentoService } from 'src/app/services/segmento.service';
 
 @Component({
   selector: 'app-segmento-delete',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SegmentoDeleteComponent implements OnInit {
 
-  constructor() { }
+  segmento: Segmento = {
+    id:         '',
+    descricao:  ''
+  }
+
+  constructor(
+    private service: SegmentoService,
+    private toast:    ToastrService,
+    private router:          Router,
+    private route:   ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.segmento.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+  findById(): void {
+    this.service.findById(this.segmento.id).subscribe(resposta => {
+      this.segmento = resposta;
+    })
+  }
+
+  delete(): void {
+    this.service.delete(this.segmento.id).subscribe(() => {
+      this.toast.success('Segmento Deletado com sucesso', 'Delete');
+      this.router.navigate(['segmento'])
+    }, ex => {
+      if(ex.error.errors) {
+        ex.error.errors.forEach(element => {
+          this.toast.error(element.message);
+        });
+      } else {
+        this.toast.error(ex.error.message);
+      }
+    })
   }
 
 }
